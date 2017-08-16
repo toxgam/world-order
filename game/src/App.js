f71003b9fdbc57c0a479e98829ec4fcc1fa6a7c2
Khac Huy Le <lekhachuy08@gmail.com>
2017-08-16 14:38:56 +1000

    Fix #2 Add basic components

--- game/src/App.js
+++ game/src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 
-class App extends Component {
+import './App.css'
+
+import GameScreen from './game_screen/GameScreen'
+
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <GameScreen 
+        width={window.innerWidth}
+        height={window.innerHeight}
+      />
+    )
   }
 }
-
-export default App;
